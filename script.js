aeb6b6d87a106685df4436b854c411322e8f97b7
Danish Imran <danish.ec33@gmail.com>
2022-10-31 14:39:15 +0530

    ready to go live

--- script.js
+++ script.js
@@ -1,36 +1,61 @@
-const body = document.getElementsByTagName('body');
+const input = document.getElementById('hello');
 const button = document.getElementsByTagName('button');
 
+const update = document.getElementsByClassName('update');
+
 function myScript(){
-   fetch('https://geo.ipify.org/api/v2/country?apiKey=at_92TwOIEOd7A5xMpqRBHJ7mBl2DSAG&ipAddress=8.8.8.8')
+     
+    const IP = input.value;
+   fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=at_92TwOIEOd7A5xMpqRBHJ7mBl2DSAG&ipAddress=${IP}`)
     .then((res)=>res.json())
-    .then((data)=> {
-        console.log(data);
-        const location = data.location;
-        const ISP = data.isp;
-        console.log(location.country + ISP);
-
-        
-    }
+    .then((data)=>setData(data)
     );
+         
+    function setData(data){
+        
 
-    
-    
+        let  data1 = [data.location.lat, data.location.lng];
+        update[0].innerHTML =data.ip;
+        update[1].innerHTML =data.location.region +', '+ data.location.country;
+        update[2].innerHTML =data.location.timezone;
+        update[3].innerHTML =data.isp;
+        if(map === undefined){
 
-    var map = L.map('map').setView([28.48, 77.01], 13);
+            var map = L.map('map').setView(data1, 13);
     const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 		maxZoom: 19,
 		attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 	}).addTo(map);
-    var marker = L.marker([28.48, 77.01]).addTo(map);
+    var marker = L.marker(data1).addTo(map);
+
+
+        }
+
+        
+
+        
+
+        
+        
+
+ 
+
+
+        
+
+
+
+
+    }
+    
+
+    
 
 
      }
 
-function hello(){
-    console.log('body');
+   
 
-}
+//window.addEventListener('load', myScript);
+button[0].addEventListener('click', myScript);
 
-
-window.addEventListener('load', myScript);
